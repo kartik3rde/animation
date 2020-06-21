fc9c1aabf4fefd214edc5e14070801ed49ff0cef
AppleHD <kartik3rde@gmail.com>
2020-06-21 12:46:03 +0530

    setup

---
+++ App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import Home from "./src/page/Home"
+
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <Home/>
+    </>
+  );
+};
+
+
+
+export default App;
